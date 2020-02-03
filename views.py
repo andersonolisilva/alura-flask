ea6018b694694df04ba619e2cbf09434134fd369
Anderson Silva <andersonolisilva@gmail.com>
2020-02-02 21:16:30 -0300

    Conclusão do Curso
    Signed-off-by: Anderson Silva <andersonolisilva@gmail.com>

---
+++ views.py
@@ -0,0 +1,98 @@
+from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
+from models import Jogo, Usuario
+from dao import JogoDao, UsuarioDao
+from jogoteca import db, app
+from helpers import deleta_arquivo, recupera_imagem
+import time
+
+jogo_dao = JogoDao(db)
+usuario_dao = UsuarioDao(db)
+
+@app.route('/')
+def index():
+    lista = jogo_dao.listar()
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('editar')))
+    jogo = jogo_dao.busca_por_id(id)
+    nome_image = recupera_imagem(id)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo,
+                           capa_jogo=nome_image or 'capa_padrao.jpg')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request. form['nome']
+    categoria = request. form['categoria']
+    console = request. form['console']
+    jogo = Jogo(nome, categoria, console)
+    jogo = jogo_dao.salvar(jogo)
+
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+
+    return redirect(url_for('index'))
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    id = request.form['id']
+    jogo = Jogo(nome, categoria, console, id)
+
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time()
+    deleta_arquivo(jogo.id)
+    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+    jogo_dao.salvar(jogo)
+    return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect( url_for('login', proxima=url_for('deletar')))
+
+    jogo_dao.deletar(id)
+    flash('Jogo apagado do sistema.')
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    usuario = usuario_dao.buscar_por_id(request.form['usuario'])
+    if usuario:
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+    else:
+        flash('Não logado, tente denovo!')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect(url_for('index'))
+
+@app.route('/imagem/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads',nome_arquivo)
